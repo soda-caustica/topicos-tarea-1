368c61299145b4963d9a89f126eb796ed1bac5b2
soda-caustica <maxbeltran@protonmail.com>
2026-09-16 19:34:20 -0300

    Se implemento linearidad de CountMin

--- count_min.hpp
+++ count_min.hpp
@@ -2,6 +2,7 @@
 #define COUNT_MIN_H
 
 #include <cstdint>
+#include <stdexcept>
 class CountMin {
 private:
   int depth;
@@ -19,25 +20,65 @@ private:
 public:
   CountMin(int d, int w) : depth{d}, width{w} {
     sketch = new int *[d];
-    for (int i = 0; i < d; i++){
+    for (int i = 0; i < d; i++) {
       sketch[i] = new int[w];
       for (int j = 0; j < w; j++)
         sketch[i][j] = 0;
     }
   }
 
-  void count(uint32_t clave){
-    for(int i = 0; i < depth; i++)
-      sketch[i][hash(i,clave)]++;
+  void count(uint32_t clave) {
+    for (int i = 0; i < depth; i++)
+      sketch[i][hash(i, clave)]++;
   }
 
-  int get(uint32_t clave){
+  int get(uint32_t clave) {
     uint32_t k = UINT32_MAX;
-    for (int i = 0; i < depth; i++){
-      int estimado = sketch[i][hash(i,clave)];
+    for (int i = 0; i < depth; i++) {
+      int estimado = sketch[i][hash(i, clave)];
       k = k < estimado ? k : estimado;
     }
     return k;
+  }
+
+  // Implementamos la suma de sketches, como las funciones de hash estan en el
+  // codigo, todos los sketches de esta clase son compatibles si sus dimensiones
+  // son iguales
+  CountMin &operator+=(const CountMin &rhs) {
+    if (depth != rhs.depth || width != rhs.width) {
+      throw std::invalid_argument("Se intentaron sumar sketches incompatibles, "
+                                  "revise las dimensiones de ambos");
+    }
+    for (int i = 0; i < depth; i++) {
+      for (int j = 0; j < width; j++) {
+        sketch[i][j] += rhs.sketch[i][j];
+      }
+    }
+    return *this;
+  }
+
+  CountMin &operator-=(const CountMin &rhs) {
+    if (depth != rhs.depth || width != rhs.width) {
+      throw std::invalid_argument(
+          "Se intentaron restar sketches incompatibles, "
+          "revise las dimensiones de ambos");
+    }
+    for (int i = 0; i < depth; i++) {
+      for (int j = 0; j < width; j++) {
+        sketch[i][j] -= rhs.sketch[i][j];
+      }
+    }
+    return *this;
+  }
+
+  // Esto es innecesario pero implentarlo es gratis
+  CountMin &operator*=(const int rhs) {
+    for (int i = 0; i < depth; i++) {
+      for (int j = 0; j < width; j++) {
+        sketch[i][j] += rhs;
+      }
+    }
+    return *this;
   }
 };
 
--- main.cpp
+++ main.cpp
@@ -4,11 +4,21 @@
 int main(){
   int d = 3;
   int w = 3;
-  int arr[] = {1,2,1,1,3,2,1,1,2,3,4,2};
-  CountMin sketch = CountMin(d,w);
-  for(int i = 0; i < 12; i++)
-    sketch.count(arr[i]);
+  int arr[]  = {1,2,1,1,3,2,1,1,2,3,4,2};
+  int arr2[] = {3,1,2,1,2,1,1,1,2,3,4,1};
+
+  CountMin sketch1 = CountMin(d,w);
+  CountMin sketch2 = CountMin(d,w);
+  CountMin sketchCombinado = CountMin(d,w);
+
+  for(int i = 0; i < 12; i++){
+    sketch1.count(arr[i]);
+    sketch2.count(arr2[i]);
+    sketchCombinado.count(arr[i]);
+    sketchCombinado.count(arr2[i]);
+  }
+  sketch1 += sketch2;
   for(int i = 1; i <= 4; i++){
-    std::cout << sketch.get(i) << '\n';
+    std::cout << sketch1.get(i) - sketchCombinado.get(i) << '\n';
   }
 }
